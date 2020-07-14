e63563f120c29b419d792090ce4fc15af44babef
Matthew Weaber <matthewweaber89@gmail.com>
2020-07-14 17:32:02 -0400

    create app and commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>This is an app to take a search and show data of a pokemon</p>
+    </div>
+  );
+}
+
+export default App;
